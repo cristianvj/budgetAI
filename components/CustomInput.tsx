4d770f330ff3335aef37d7c3273659ea76b20376
unknown <cristiandavid.vj@gmail.com>
2024-12-10 23:21:22 -0500

    Fixed translations and eslint errors

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Control, FieldPath } from 'react-hook-form';
+import { z } from 'zod';
+
+import { authFormSchema } from '@/lib/utils';
 import { FormControl, FormField, FormLabel, FormMessage } from './ui/form';
 import { Input } from './ui/input';
 
-import { Control, FieldPath } from 'react-hook-form';
-import { z } from 'zod';
-import { authFormSchema } from '@/lib/utils';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const formSchema = authFormSchema('sign-up');
@@ -16,32 +17,29 @@ interface CustomInput {
   placeholder: string
 } 
 
-const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
-
-  return (
-    <FormField
-      control={control}
-      name={name}
-      render={({ field }) => (
-        <div className="form-item">
-          <FormLabel className="form-label">
-            {label}
-          </FormLabel>
-          <div className="flex w-full flex-col">
-            <FormControl>
-              <Input 
-                placeholder={placeholder}
-                className="input-class"
-                type={name === 'password' ? 'password' : 'text'}
-                {...field}
-              />
-            </FormControl>
-            <FormMessage className="form-message mt-2" />
-          </div>
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => (
+  <FormField
+    control={control}
+    name={name}
+    render={({ field }) => (
+      <div className="form-item">
+        <FormLabel className="form-label">
+          {label}
+        </FormLabel>
+        <div className="flex w-full flex-col">
+          <FormControl>
+            <Input 
+              placeholder={placeholder}
+              className="input-class"
+              type={name === 'password' ? 'password' : 'text'}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage className="form-message mt-2" />
         </div>
-      )}
-    />
-  );
-};
+      </div>
+    )}
+  />
+);
 
 export default CustomInput;
