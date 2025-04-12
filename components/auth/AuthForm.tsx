ccfb98f0850798db6188cbb9ba646ea8fd4b4ed1
unknown <cristiandavid.vj@gmail.com>
2025-04-12 12:32:30 -0500

    Fix login - register - social login  process

--- components/auth/AuthForm.tsx
+++ components/auth/AuthForm.tsx
@@ -14,13 +14,19 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { signIn, signUp, signUpWithGoogle } from '@/lib/actions/user.actions';
 
 import CustomInput from '../common/CustomInput';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { authFormSchema } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import { Form } from '@/components/ui/form';
 import { Icons } from '@/components/ui/icons';
 
-const AuthForm = ({type}: Readonly<{type: string}>) => {
+const AuthForm = ({ type }: Readonly<{ type: string }>) => {
   const router = useRouter();
   const { toast } = useToast();
 
@@ -38,8 +44,8 @@ const AuthForm = ({type}: Readonly<{type: string}>) => {
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
     setIsLoading(true);
-    try {   
-      if(type === 'sign-up') {
+    try {
+      if (type === 'sign-up') {
         const userData = {
           firstName: data.firstName!,
           lastName: data.lastName!,
@@ -50,18 +56,18 @@ const AuthForm = ({type}: Readonly<{type: string}>) => {
           dateOfBirth: data.dateOfBirth!,
           ssn: data.ssn!,
           email: data.email,
-          password: data.password
+          password: data.password,
         };
         const response = await signUp(userData);
         if (response) router.push('/dashboard');
       }
 
-      if(type === 'sign-in') {
+      if (type === 'sign-in') {
         const response = await signIn({
           email: data.email,
           password: data.password,
         });
-        if(response) router.push('/dashboard');
+        if (response) router.push('/dashboard');
       }
     } catch (error) {
       console.log(error);
@@ -92,67 +98,138 @@ const AuthForm = ({type}: Readonly<{type: string}>) => {
   };
 
   return (
-    <div className='flex flex-col w-full items-center h-full overflow-hidden'>
-      <div className='overflow-y-auto h-full w-full p-6 lg:p-12 flex justify-center flex-col items-center'>
-      <h1 className='mt-6 text-3xl font-extrabold text-center lg:hidden'>💰BudgetAI</h1>
-        <Card className={`w-full max-w-md mt-3 ${type === 'sign-up' ? 'lg:mt-40' : ''} max-h-max`}>
+    <div className="flex flex-col w-full items-center h-full overflow-hidden">
+      <div className="overflow-y-auto h-full w-full p-6 lg:p-12 flex justify-center flex-col items-center">
+        <h1 className="mt-6 text-3xl font-extrabold text-center lg:hidden">
+          💰BudgetAI
+        </h1>
+        <Card
+          className={`w-full max-w-md mt-3 ${
+            type === 'sign-up' ? 'lg:mt-40' : ''
+          } max-h-max`}
+        >
           <CardHeader>
-            <CardTitle className='text-2xl text-center'>{type === 'sign-in' ? 'Iniciar Sesión' : 'Registro'}</CardTitle>
+            <CardTitle className="text-2xl text-center">
+              {type === 'sign-in' ? 'Iniciar Sesión' : 'Registro'}
+            </CardTitle>
           </CardHeader>
           <CardContent>
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)}>
-                <div className='space-y-4'>
-                {type === 'sign-up' && (
+                <div className="space-y-4">
+                  {type === 'sign-up' && (
                     <>
-                      <div className='flex gap-4'>
-                        <CustomInput control={form.control} name='firstName' label='Nombres' placeholder='Ingresa tu nombre' />
-                        <CustomInput control={form.control} name='lastName' label='Apellidos' placeholder='Ingresa tu apellido' />
+                      <div className="flex gap-4">
+                        <CustomInput
+                          control={form.control}
+                          name="firstName"
+                          label="Nombres"
+                          placeholder="Ingresa tu nombre"
+                        />
+                        <CustomInput
+                          control={form.control}
+                          name="lastName"
+                          label="Apellidos"
+                          placeholder="Ingresa tu apellido"
+                        />
                       </div>
-                      <CustomInput control={form.control} name='address' label='Dirección' placeholder='Ingresa una dirección específica' />
-                      <CustomInput control={form.control} name='city' label='Ciudad' placeholder='Ingresa tu ciodad' />
-                      <div className='flex gap-4'>
-                        <CustomInput control={form.control} name='state' label='Estado o Departamento' placeholder='Ejemplo: NY' />
-                        <CustomInput control={form.control} name='postalCode' label='Código postal' placeholder='Ejemplo: 11101' />
+                      <CustomInput
+                        control={form.control}
+                        name="address"
+                        label="Dirección"
+                        placeholder="Ingresa una dirección específica"
+                      />
+                      <CustomInput
+                        control={form.control}
+                        name="city"
+                        label="Ciudad"
+                        placeholder="Ingresa tu ciodad"
+                      />
+                      <div className="flex gap-4">
+                        <CustomInput
+                          control={form.control}
+                          name="state"
+                          label="Estado o Departamento"
+                          placeholder="Ejemplo: NY"
+                        />
+                        <CustomInput
+                          control={form.control}
+                          name="postalCode"
+                          label="Código postal"
+                          placeholder="Ejemplo: 11101"
+                        />
                       </div>
-                      <div className='flex gap-4'>
-                        <CustomInput control={form.control} name='dateOfBirth' label='Fecha de nacimiento' placeholder='YYYY-MM-DD' />
-                        <CustomInput control={form.control} name='ssn' label='Documento o ID' placeholder='Ejemplo: 1234' />
+                      <div className="flex gap-4">
+                        <CustomInput
+                          control={form.control}
+                          name="dateOfBirth"
+                          label="Fecha de nacimiento"
+                          placeholder="YYYY-MM-DD"
+                        />
+                        <CustomInput
+                          control={form.control}
+                          name="ssn"
+                          label="Documento o ID"
+                          placeholder="Ejemplo: 1234"
+                        />
                       </div>
                     </>
                   )}
-                  <CustomInput control={form.control} name='email' label='Email' placeholder='Enter your email' />
-                  <CustomInput control={form.control} name='password' label='Password' placeholder='Enter your password' />
+                  <CustomInput
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="Enter your email"
+                  />
+                  <CustomInput
+                    control={form.control}
+                    name="password"
+                    label="Password"
+                    placeholder="Enter your password"
+                  />
 
-                  <Button className='w-full' type='submit' disabled={isLoading}>
+                  <Button className="w-full" type="submit" disabled={isLoading}>
                     {isLoading && (
-                      <Icons.spinner className='mr-2 h-4 w-4 animate-spin' />
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
                     )}
                     {type === 'sign-in' ? 'Inicia Sesión' : 'Registrarse'}
                   </Button>
-                  <div className='relative'>
-                    <div className='absolute inset-0 flex items-center'>
-                      <span className='w-full border-t' />
+                  <div className="relative">
+                    <div className="absolute inset-0 flex items-center">
+                      <span className="w-full border-t" />
                     </div>
-                    <div className='relative flex justify-center text-xs uppercase'>
-                      <span className='bg-background px-2 text-muted-foreground'>
-                        O continúa con: 
+                    <div className="relative flex justify-center text-xs uppercase">
+                      <span className="bg-background px-2 text-muted-foreground">
+                        O continúa con:
                       </span>
                     </div>
                   </div>
-                  <Button disabled={isLoading} variant='outline' type='button' className='w-full' onClick={handleSignUpWithGoogle}>
-                    <FcGoogle className='mr-2 h-4 w-4' />
-                    {type === 'sign-in' ? 'Inicia sesión con Google' : 'Regístrate con Google'  }
+                  <Button
+                    disabled={isLoading}
+                    variant="outline"
+                    type="button"
+                    className="w-full"
+                    onClick={handleSignUpWithGoogle}
+                  >
+                    <FcGoogle className="mr-2 h-4 w-4" />
+                    {type === 'sign-in'
+                      ? 'Inicia sesión con Google'
+                      : 'Regístrate con Google'}
                   </Button>
                 </div>
               </form>
             </Form>
           </CardContent>
-          <CardFooter className='flex justify-center pt-4'>
-            <p className='text-sm text-gray-600'>
-              {type === 'sign-in' ? '¿Aún no tienes una cuenta?' : '¿Ya tienes una cuenta?'}{' '}
-              <Link className='font-medium text-primary hover:underline' href={type === 'sign-in' ? '/sign-up' : 'sign-in'}  >
-              {type === 'sign-in' ? 'Regístrate' : 'Iniciar Sesión'}
+          <CardFooter className="flex justify-center pt-4">
+            <p className="text-sm text-gray-600">
+              {type === 'sign-in'
+                ? '¿Aún no tienes una cuenta?'
+                : '¿Ya tienes una cuenta?'}{' '}
+              <Link
+                className="font-medium text-primary hover:underline"
+                href={type === 'sign-in' ? '/sign-up' : 'sign-in'}
+              >
+                {type === 'sign-in' ? 'Regístrate' : 'Iniciar Sesión'}
               </Link>
             </p>
           </CardFooter>
--- lib/actions/user.actions.tsx
+++ lib/actions/user.actions.tsx
@@ -2,10 +2,8 @@
 
 import { cookies, headers } from 'next/headers';
 import { redirect } from 'next/navigation';
-
 import { ID, OAuthProvider, Query } from 'node-appwrite';
 import { createAdminClient, createSessionClient } from '../appwrite';
-
 import { parseStringify } from '../utils';
 
 const {
@@ -20,7 +18,7 @@ export const getUserInfo = async ({ userId }: getUserInfoProps) => {
     const user = await database.listDocuments(
       DATABASE_ID!,
       USER_COLLECTION_ID!,
-      [Query.equal('userId', [userId])]
+      [Query.equal('userId', [userId])],
     );
 
     return parseStringify(user.documents[0]);
@@ -29,8 +27,8 @@ export const getUserInfo = async ({ userId }: getUserInfoProps) => {
   }
 };
 
-export const signIn = async ({email, password}: SignInProps) => {
- try {
+export const signIn = async ({ email, password }: SignInProps) => {
+  try {
     const { account } = await createAdminClient();
     const user = await account.createEmailPasswordSession(email, password);
 
@@ -49,14 +47,24 @@ export const signIn = async ({email, password}: SignInProps) => {
   }
 };
 
-export const signUp = async ({password, ...userData}: SignUpParams) => {
+export const signUp = async ({ password, ...userData }: SignUpParams) => {
   const { email, firstName, lastName } = userData;
-
   let newUserAccount;
 
   try {
     const { account, database } = await createAdminClient();
+    // Check if a user with the given email already exists
+    const existingUsers = await database.listDocuments(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      [Query.equal('email', email)],
+    );
 
+    if (existingUsers.documents.length > 0) {
+      throw new Error('A user with this email already exists.');
+    }
+
+    // Create a new user account
     newUserAccount = await account.create(
       ID.unique(),
       email,
@@ -64,7 +72,7 @@ export const signUp = async ({password, ...userData}: SignUpParams) => {
       `${firstName} ${lastName}`,
     );
 
-    if(!newUserAccount) throw new Error('Error creating user');
+    if (!newUserAccount) throw new Error('Error creating user');
 
     const newUser = await database.createDocument(
       DATABASE_ID!,
@@ -73,7 +81,7 @@ export const signUp = async ({password, ...userData}: SignUpParams) => {
       {
         ...userData,
         userId: newUserAccount.$id,
-      }
+      },
     );
 
     const session = await account.createEmailPasswordSession(email, password);
@@ -88,6 +96,36 @@ export const signUp = async ({password, ...userData}: SignUpParams) => {
     return parseStringify(newUser);
   } catch (error) {
     console.log('error: ', error);
+    throw error;
+  }
+};
+
+export const registerSocialUser = async (userData: User) => {
+  try {
+    const { database } = await createAdminClient();
+    // Check if a user with the given email already exists
+    const existingUsers = await database.listDocuments(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      [Query.equal('email', userData.email)],
+    );
+    if (existingUsers.documents.length > 0) {
+      console.log('User with this email already exists.');
+      return; // Exit the function if the user already exists
+    }
+    // Create a new user document
+    await database.createDocument(
+      DATABASE_ID!,
+      USER_COLLECTION_ID!,
+      ID.unique(),
+      {
+        userId: userData.$id,
+        email: userData.email,
+        firstName: userData?.firstName || userData.name,
+      },
+    );
+  } catch (error) {
+    console.log('error: ', error); // Fixed singlequote issue
   }
 };
 
@@ -107,6 +145,8 @@ export async function getLoggedInUser() {
         name: result.name,
       };
     }
+
+    registerSocialUser(user);
     return parseStringify(user);
   } catch (error) {
     console.log('error: ', error);
@@ -116,23 +156,18 @@ export async function getLoggedInUser() {
 
 export async function signOut() {
   const { account } = await createSessionClient();
-
   (await cookies()).delete('appwrite-session');
   await account.deleteSession('current');
-
   redirect('/sign-in');
 }
 
 export async function signUpWithGoogle() {
   const { account } = await createAdminClient();
-
   const origin = (await headers()).get('origin');
-  
-	const redirectUrl = await account.createOAuth2Token(
+  const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
     `${origin}/api/oauth`,
-    `${origin}/sign-in`
+    `${origin}/sign-in`,
   );
-
-	redirect(redirectUrl);
+  redirect(redirectUrl);
 }
